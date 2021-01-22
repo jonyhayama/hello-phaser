311e46ad7ec136a5be65d790a33fba034f023d85
Jony Hayama <jonathan.hayama@gmail.com>
2021-01-22 19:11:06 -0300

    adds player and platforms

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,17 +1,24 @@
 import Phaser from 'phaser'
 
+const KEYS = {
+  GROUND: 'ground',
+  DUDE: 'dude'
+}
+
 export default class MainScene extends Phaser.Scene {
   constructor() {
     super('main-scene')
+
+    this.player = undefined;
   }
 
   preload() {
     this.load.image('bomb', 'assets/img/bomb.png');
-    this.load.image('platform', 'assets/img/platform.png');
+    this.load.image(KEYS.GROUND, 'assets/img/platform.png');
     this.load.image('sky', 'assets/img/sky.png');
     this.load.image('star', 'assets/img/star.png');
     
-    this.load.spritesheet('dude', 'assets/img/dude.png', {
+    this.load.spritesheet(KEYS.DUDE, 'assets/img/dude.png', {
       frameWidth: 32,
       frameHeight: 48
     });
@@ -20,5 +27,44 @@ export default class MainScene extends Phaser.Scene {
   create() {
     this.add.image(400, 300, 'sky');
     this.add.image(400, 300, 'star');
+
+    this.createPlatforms();
+    this.createPlayer();
+  }
+
+  createPlatforms(){
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, KEYS.GROUND).setScale(2).refreshBody();
+    
+    platforms.create(600, 400, KEYS.GROUND);
+    platforms.create(50, 250, KEYS.GROUND);
+    platforms.create(750, 220, KEYS.GROUND);
+  }
+
+  createPlayer(){
+    this.player = this.physics.add.sprite(100, 450, KEYS.DUDE)
+		this.player.setBounce(0.2)
+		this.player.setCollideWorldBounds(true)
+
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers(KEYS.DUDE, { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		})
+		
+		this.anims.create({
+			key: 'turn',
+			frames: [ { key: KEYS.DUDE, frame: 4 } ],
+			frameRate: 20
+		})
+		
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers(KEYS.DUDE, { start: 5, end: 8 }),
+			frameRate: 10,
+			repeat: -1
+		})
   }
 }
