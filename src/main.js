3a4156ec7dd3535e9ee155ba9339909d875e32de
Jony Hayama <jonathan.hayama@gmail.com>
2021-01-29 18:28:41 -0300

    remember user debug mode selection

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 
 import MainScene from './scenes/MainScene'
 const merge = require('deepmerge')
-
+let debugMode = JSON.parse( localStorage.getItem('@helloPhaser/structure/debugMode') );
 const config = {
 	type: Phaser.AUTO,
 	width: 800,
@@ -11,7 +11,8 @@ const config = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 300 }
+			gravity: { y: 300 },
+			debug: debugMode
 		}
 	},
 	scene: [MainScene]
@@ -20,10 +21,11 @@ const config = {
 const APP = {
 	game: new Phaser.Game(config)
 }
-
+document.getElementById('game-debug-toggle').checked = debugMode;
 document.getElementById('game-debug-toggle').addEventListener('change', function(e){
 	e.preventDefault();
 	let debug = e.target.checked;
+	localStorage.setItem('@helloPhaser/structure/debugMode', JSON.stringify(debug));
 	
 	APP.game.destroy(true);
 	APP.game = new Phaser.Game(merge(config, { physics: { arcade: { debug } } }))
