8c21912dc62cfbe31cc06e09e58ec7939ffa2d36
Jony Hayama <jonathan.hayama@gmail.com>
2021-02-11 10:58:19 -0300

    make stars spawn less often

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -316,7 +316,15 @@ export default class MainScene extends Phaser.Scene {
 
     this.bombSpawner.spawn(player.x)
 
-    this.starSpawner.spawn(player.x)
+    const getRandomInt = function (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    if( getRandomInt(1, 10) === 1 ){
+      this.starSpawner.spawn(player.x)
+    }
   }
 
   hitBomb(player, bomb){
